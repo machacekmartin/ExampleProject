cebb493b010dd33fea1efbb1e2cef985c4c3a8f4
Martin <machacek-martin@seznam.com>
2020-05-06 13:13:08 +0200

    required inputs fixed

--- resources/views/images/create.php
+++ resources/views/images/create.php
@@ -1,8 +1,8 @@
 <h1 class="heading">Upload new image</h1>
 <form method="POST" enctype="multipart/form-data" class="create-form image-form">
-    <input type="text" name="header" placeholder="Image title" class="header input-text transition" maxlength="30">
+    <input type="text" name="header" placeholder="Image title" class="header input-text transition" maxlength="30" required>
     <div class="image transition">
-        <input type="file" name="image" class="file" onchange="previewUpload(event)">
+        <input type="file" name="image" class="file" onchange="previewUpload(event)" required>
     </div>
     <div class="result flex row">
         <input type="submit" value="Upload" class="button transition">
--- resources/views/posts/create.php
+++ resources/views/posts/create.php
@@ -1,7 +1,7 @@
 <h1 class="heading">Create new post</h1>
 <form method="POST" class="create-form post-form flex column">
-    <input type="text" name="header" placeholder="Title" class="input-text transition" maxlength="40">
-    <textarea name="content" placeholder="Contents of your post" class="input-text transition" rows="20" maxlength="750" minlength="50"></textarea>
+    <input type="text" name="header" placeholder="Title" class="input-text transition" maxlength="40" required>
+    <textarea name="content" placeholder="Contents of your post" class="input-text transition" rows="20" maxlength="750" minlength="50" required></textarea>
     <input type="submit" value="Create new post" class="button transition">
     <input type="hidden" name="action" value="posts">
 </form>
